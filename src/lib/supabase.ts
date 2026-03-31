174092deacaec889eb6c0799eea2d7f8cea4bfde
Leonardo Coradi <clashonpq@gmail.com>
2026-03-30 23:06:12 -0300

    supad

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jvagwkcytqjiyhnsuugn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2YWd3a2N5dHFqaXlobnN1dWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNzI3NzcsImV4cCI6MjA1ODk0ODc3N30.TfrJ7yJRU5i66qFIoBFI-w_DmAPBiRB';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://jvagwkcytqjiyhnsuugn.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2YWd3a2N5dHFqaXlobnN1dWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNzI3NzcsImV4cCI6MjA1ODk0ODc3N30.TfrJ7yJRU5i66qFIoBFI-w_DmAPBiRB';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
